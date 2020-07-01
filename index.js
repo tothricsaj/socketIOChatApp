a5e9e1ac512f578d4c2ebc3c7d39b7b4b06167d6
tothricsaj <tothricsaj@gmail.com>
2020-07-01 15:56:02 +0200

    messeging between clients

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ io.on('connection', (socket) => {
 
     socket.on('disconnect', () => console.log('user disconnected'))
     socket.on('chat message', (msg) => {
-        console.log(`message -> ${msg}`)
+        io.emit('chat message', msg)
     })
 })
 

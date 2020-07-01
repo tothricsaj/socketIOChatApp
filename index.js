1b87b9f6237192f52e006a45c012ff3de3df92d3
tothricsaj <tothricsaj@gmail.com>
2020-07-01 15:31:29 +0200

    messaging to server :smile:

--- index.js
+++ index.js
@@ -6,6 +6,13 @@ app.get('/', (req, res) => {
     res.sendFile(`${__dirname}/index.html`)
 })
 
-io.on('connection', (socket) => console.log('a user connected'))
+io.on('connection', (socket) => {
+    console.log('a user connected')
+
+    socket.on('disconnect', () => console.log('user disconnected'))
+    socket.on('chat message', (msg) => {
+        console.log(`message -> ${msg}`)
+    })
+})
 
 http.listen(3000, () => console.log('listening on *:3000'))

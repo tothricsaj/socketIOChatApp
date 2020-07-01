b1ea05a31334ff95941870cbf4e1829119fc8dec
tothricsaj <tothricsaj@gmail.com>
2020-07-01 14:49:37 +0200

    html is added

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express')
 const app = express()
 
 app.get('/', (req, res) => {
-    res.send('<h1>Hello SocketIO</h1>')
+    res.sendFile(`${__dirname}/index.html`)
 })
 
 app.listen(3000, () => console.log('listening on *:3000'))

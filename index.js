50978487d0004e50d3fc84f586af16c83da2d9ae
tothricsaj <tothricsaj@gmail.com>
2020-07-01 15:14:29 +0200

    socket io is added to project

--- index.js
+++ index.js
@@ -1,8 +1,11 @@
-const express = require('express')
-const app = express()
+const app = require('express')()
+const http = require('http').createServer(app)
+const io = require('socket.io')(http)
 
 app.get('/', (req, res) => {
     res.sendFile(`${__dirname}/index.html`)
 })
 
-app.listen(3000, () => console.log('listening on *:3000'))
+io.on('connection', (socket) => console.log('a user connected'))
+
+http.listen(3000, () => console.log('listening on *:3000'))
